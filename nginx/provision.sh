49b2f5479ca835fef3f8f33ef4f2eb080fc20641
Vytenis Bivainis <monrealis@gmail.com>
2017-08-31 23:59:21 +0300

    Copy sites-enabled

--- nginx/provision.sh
+++ nginx/provision.sh
@@ -2,6 +2,9 @@ apt-get -y update
 
 apt-get -y install nginx
 
+rm -rf /etc/nginx/sites-enabled;
+cp -r /vagrant/sites-enabled/ /etc/nginx
+
 rm -rf /usr/share/nginx/www/
 ln -s /vagrant/www /usr/share/nginx/www
 
